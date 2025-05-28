25cf8c99da3dc53c9fe26170635052d58f2fd8e8
김기진 <abjin69@gmail.com>
2025-05-28 10:18:34 +0900

    5-28

---
+++ echo_client_web_get.py
@@ -0,0 +1,25 @@
+import socket
+
+HOST = 'localhost'
+PORT = 10005
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    # 요청 문자열 구성
+    request = (
+        "GET /greet?username=kevin HTTP/1.1\r\n"
+        f"Host: {HOST}:{PORT}\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+    )
+    s.sendall(request.encode('utf-8'))
+
+    # 응답 읽기
+    response = b''
+    while True:
+        chunk = s.recv(4096)
+        if not chunk:
+            break
+        response += chunk
+
+    print(response.decode('utf-8'))
